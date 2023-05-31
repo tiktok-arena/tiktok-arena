f9e4114fdd4f51998303a74285c9c33a86068d18
Masedko <andruugladkuu@gmail.com>
2023-05-31 13:25:04 +0300

    hotfix

--- internal/api/routers/tournament.router.go
+++ internal/api/routers/tournament.router.go
@@ -9,7 +9,7 @@ import (
 func NewTournamentRouter(c *controllers.TournamentController) func(router fiber.Router) {
 	return func(router fiber.Router) {
 		router.Get("", c.GetAllTournaments)
-		router.Get("/contests/:tournamentId", c.GetTournamentContest)
+		router.Get("/contest/:tournamentId", c.GetTournamentContest)
 		router.Post("/create", middleware.Protected(), c.CreateTournament)
 		router.Put("/edit/:tournamentId", middleware.Protected(), c.EditTournament)
 		router.Delete("/delete/:tournamentId", middleware.Protected(), c.DeleteTournament)
