697992e1988f375775e67b7b75a20d014aa415a1
Masedko <andruugladkuu@gmail.com>
2023-07-10 20:37:39 +0300

    Fixed swagger issues

--- internal/api/controllers/tournament.controller.go
+++ internal/api/controllers/tournament.controller.go
@@ -170,7 +170,7 @@ func (cr *TournamentController) DeleteTournaments(c *fiber.Ctx) error {
 //	@Param			search				query		string						false	"search"
 //	@Success		200					{array}		dtos.TournamentsResponse	"Contest bracket"
 //	@Failure		400					{object}	dtos.MessageResponseType	"Failed to return tournament contests"
-//	@Router			/tournament [get]																																																																																																																																																																																																																																				[get]
+//	@Router			/tournament [get]																																																																																																																																																																																																																																												[get]
 func (cr *TournamentController) GetAllTournaments(c *fiber.Ctx) error {
 	q := new(dtos.PaginationQueries)
 	if err := c.QueryParser(q); err != nil {
